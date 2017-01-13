22d5a35e402e9f23731c29b4b6d3901f7d486bb1
nsbradford <nsbradford@gmail.com>
2017-01-12 21:33:35 -0500

    Initial commit for HW1; add template.

---
+++ HW1/homework1.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def problem1 (A, B):
+	return A + B
+
+def problem2 (A, B, C):
+	return ...
+
+def problem3 (A, B, C):
+	return ...
+
+def problem4 (x, y):
+	return ...
+
+def problem5 (A):
+	return ...
+
+def problem6 (A):
+	return ...
+
+def problem7 (A):
+	return ...
+
+def problem8 (A, x):
+	return ...
+
+def problem9 (A, x):
+	return ...
+
+def problem10 (A, alpha):
+	return ...
+
+def problem11 (A, i, j):
+	return ...
+
+def problem12 (A, i):
+	return ...
+
+def problem13 (A, c, d):
+	return ...
+
+def problem14 (A, k):
+	return ...
+
+def problem15 (x, k, m, s):
+	return ...
