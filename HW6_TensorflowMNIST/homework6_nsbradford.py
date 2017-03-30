a619c77f2db3751df149d45c75e2aea01b87d9f3
nsbradford <nsbradford@gmail.com>
2017-03-30 17:23:46 -0400

    Finish hw6 potentially...

--- HW6_TensorflowMNIST/homework6_nsbradford.py
+++ HW6_TensorflowMNIST/homework6_nsbradford.py
@@ -30,14 +30,97 @@ import tensorflow as tf
 
 FLAGS = None
 
-def accuracy(sess, x, y, y_, test_data, test_labels):
+def accuracy(text, sess, x, y, y_, keep_prob, test_data, test_labels):
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(sess.run(accuracy, feed_dict={x: test_data,
-                                        y_: test_labels}))    
+    print(text, ':', sess.run(accuracy, feed_dict={x: test_data,  
+                                        y_: test_labels,
+                                        keep_prob: 1.0 }))
 
 def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+    # ============================================
+    # model with 98.2% accuracy
+    # # Create the model
+    # SIZE_H1 = 300
+    # SIZE_H2 = 40
+    # SIZE_H3 = 20
+    # SIZE_OUTPUT = 10
+    # SIZE_INPUT = 784
+    # LEARNING_RATE = 0.5
+    # N_BATCHES = 10000
+
+    # keep_prob = tf.placeholder(tf.float32)
+    # x = tf.placeholder(tf.float32, [None, SIZE_INPUT])
+    # W1 = init_weights([SIZE_INPUT, SIZE_H1])
+    # b1 = init_weights([SIZE_H1])
+    # W2 = init_weights([SIZE_H1, SIZE_H2])
+    # b2 = init_weights([SIZE_H2])
+    # W3 = init_weights([SIZE_H2, SIZE_OUTPUT])
+    # b3 = init_weights([SIZE_OUTPUT])
+
+    # h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+    # h1_drop = tf.nn.dropout(h1, keep_prob)
+    # h2 = tf.nn.relu(tf.matmul(h1_drop, W2) + b2)
+    # h2_drop = tf.nn.dropout(h2, keep_prob)
+    # y = tf.matmul(h2_drop, W3) + b3
+
+
+    # ============================================
+    # model with 98.32% accuracy
+    # SIZE_H1 = 300
+    # SIZE_H2 = 40
+    # SIZE_H3 = 20
+    # SIZE_OUTPUT = 10
+    # SIZE_INPUT = 784
+    # LEARNING_RATE = 0.4
+    # N_BATCHES = 10000
+    # BATCH_SIZE = 124
+
+    # keep_prob = tf.placeholder(tf.float32)
+    # x = tf.placeholder(tf.float32, [None, SIZE_INPUT])
+    # W1 = init_weights([SIZE_INPUT, SIZE_H1])
+    # b1 = init_weights([SIZE_H1])
+    # W2 = init_weights([SIZE_H1, SIZE_H2])
+    # b2 = init_weights([SIZE_H2])
+    # W3 = init_weights([SIZE_H2, SIZE_OUTPUT])
+    # b3 = init_weights([SIZE_OUTPUT])
+
+    # h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+    # # h1_drop = tf.nn.dropout(h1, keep_prob)
+    # h2 = tf.nn.relu(tf.matmul(h1, W2) + b2)
+    # # h2_drop = tf.nn.dropout(h2, keep_prob)
+    # y = tf.matmul(h2, W3) + b3
+
+    # ============================================
+    # Create the model
+    # SIZE_H1 = 300
+    # SIZE_H2 = 100
+    # SIZE_H3 = 40
+    # SIZE_OUTPUT = 10
+    # SIZE_INPUT = 784
+    # LEARNING_RATE = 0.4
+    # N_BATCHES = 10000
+    # BATCH_SIZE = 124
+
+    # keep_prob = tf.placeholder(tf.float32)
+    # x = tf.placeholder(tf.float32, [None, SIZE_INPUT])
+    # W1 = init_weights([SIZE_INPUT, SIZE_H1])
+    # b1 = init_weights([SIZE_H1])
+    # W2 = init_weights([SIZE_H1, SIZE_H2])
+    # b2 = init_weights([SIZE_H2])
+    # W3 = init_weights([SIZE_H2, SIZE_H3])
+    # b3 = init_weights([SIZE_H3])
+    # W4 = init_weights([SIZE_H3, SIZE_OUTPUT])
+    # b4 = init_weights([SIZE_OUTPUT])
+
+    # h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+    # h1_drop = tf.nn.dropout(h1, keep_prob)
+    # h2 = tf.nn.relu(tf.matmul(h1_drop, W2) + b2)
+    # h2_drop = tf.nn.dropout(h2, keep_prob)
+    # h3 = tf.nn.relu(tf.matmul(h2_drop, W3) + b3)
+    # y = tf.matmul(h3, W4) + b4
 
 def main(_):
     """
@@ -47,39 +130,55 @@ def main(_):
             .updater(Updater.NESTEROVS).momentum(0.9)
             .regularization(true).l2(1e-4)
     """
-    # Create the model
-    x = tf.placeholder(tf.float32, [None, 784])
-    W1 = init_weights([784, 200])
-    b1 = init_weights([200])
+    # model with 98.32% accuracy
+    SIZE_H1 = 300
+    SIZE_H2 = 40
+    SIZE_H3 = 20
+    SIZE_OUTPUT = 10
+    SIZE_INPUT = 784
+    LEARNING_RATE = 0.4
+    N_BATCHES = 10000
+    BATCH_SIZE = 124
+
+    keep_prob = tf.placeholder(tf.float32)
+    x = tf.placeholder(tf.float32, [None, SIZE_INPUT])
+    W1 = init_weights([SIZE_INPUT, SIZE_H1])
+    b1 = init_weights([SIZE_H1])
+    W2 = init_weights([SIZE_H1, SIZE_H2])
+    b2 = init_weights([SIZE_H2])
+    W3 = init_weights([SIZE_H2, SIZE_OUTPUT])
+    b3 = init_weights([SIZE_OUTPUT])
+
     h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
-    W2 = init_weights([200, 40])
-    b2 = init_weights([40])
+    # h1_drop = tf.nn.dropout(h1, keep_prob)
     h2 = tf.nn.relu(tf.matmul(h1, W2) + b2)
-    W3 = init_weights([40, 10])
-    b3 = init_weights([10])
+    # h2_drop = tf.nn.dropout(h2, keep_prob)
     y = tf.matmul(h2, W3) + b3
 
     # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
     cross_entropy = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    train_step = tf.train.GradientDescentOptimizer(.1).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
 
     sess = tf.InteractiveSession()# with tf.InteractiveSession() as sess:
     tf.global_variables_initializer().run()
+    
     # Train
-    N_BATCHES = 10000
     for i in range(N_BATCHES):
-        batch_xs, batch_ys = mnist.train.next_batch(256)
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-        if i % 200 == 0:
-            accuracy(sess, x, y, y_, test_data=mnist.test.images, test_labels=mnist.test.labels)
+        batch_xs, batch_ys = mnist.train.next_batch(BATCH_SIZE)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+        text = str(i+1) + ' Train'
+        if i > N_BATCHES - 21 or i % 200 == 0:
+            accuracy(text, sess, x, y, y_, keep_prob, test_data=mnist.train.images, 
+                                test_labels=mnist.train.labels)
 
     # Test trained model
-    accuracy(sess, x, y, y_, test_data=mnist.test.images, test_labels=mnist.test.labels)
-    print('Reset TF graph...')
-    sess.close()
-    tf.reset_default_graph()
+    text = 'Final accuracy'
+    accuracy(text, sess, x, y, y_, keep_prob, test_data=mnist.test.images, test_labels=mnist.test.labels)
+    # print('Reset TF graph...')
+    # sess.close()
+    # tf.reset_default_graph()
     print('Successful exit')
 
 
